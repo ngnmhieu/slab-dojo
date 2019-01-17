673a6132cb22d64a2b5d0f8293a311f9683d723c
Mirco Heinzelmann <mirco.heinzelmann@iteratec.de>
2019-01-17 15:33:29 +0100

    EDO-162 add missing import

--- src/main/java/de/otto/teamdojo/web/rest/ImageResource.java
+++ src/main/java/de/otto/teamdojo/web/rest/ImageResource.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
