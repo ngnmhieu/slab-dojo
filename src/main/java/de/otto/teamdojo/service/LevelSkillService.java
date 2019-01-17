afac2931b1d1efb7a811686d9b80c6fe9e03f692
Mirco Heinzelmann <mirco.heinzelmann@iteratec.de>
2019-01-17 17:14:56 +0100

    EDO-162: add missing imports

--- src/main/java/de/otto/teamdojo/service/LevelSkillService.java
+++ src/main/java/de/otto/teamdojo/service/LevelSkillService.java
@@ -5,6 +5,7 @@ import de.otto.teamdojo.service.dto.LevelSkillDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
--- src/main/java/de/otto/teamdojo/service/impl/LevelSkillServiceImpl.java
+++ src/main/java/de/otto/teamdojo/service/impl/LevelSkillServiceImpl.java
@@ -1,19 +1,21 @@
 package de.otto.teamdojo.service.impl;
 
-import de.otto.teamdojo.service.LevelSkillService;
 import de.otto.teamdojo.domain.LevelSkill;
 import de.otto.teamdojo.repository.LevelSkillRepository;
+import de.otto.teamdojo.service.LevelSkillService;
 import de.otto.teamdojo.service.dto.LevelSkillDTO;
 import de.otto.teamdojo.service.mapper.LevelSkillMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Service Implementation for managing LevelSkill.
