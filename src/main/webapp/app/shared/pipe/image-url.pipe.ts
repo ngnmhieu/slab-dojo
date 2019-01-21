f70bef0b1ccd59bd36e42738e0c1528853878504
Hieu Nguyen <ngnmhieu@gmail.com>
2019-01-21 17:27:51 +0100

    [EDO-138] only set cacheBuster if provided

--- src/main/webapp/app/shared/pipe/image-url.pipe.ts
+++ src/main/webapp/app/shared/pipe/image-url.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'imageUrl' })
 export class ImageUrlPipe implements PipeTransform {
     transform(imageId: string | number, size: string, cacheBuster?: string): string {
-        return imageId ? `/api/images/${imageId}/content?size=${size}&cacheBuster=${cacheBuster}` : '';
+        const cacheBusterParam = cacheBuster ? `&cacheBuster=${cacheBuster}` : '';
+        return imageId ? `/api/images/${imageId}/content?size=${size}${cacheBusterParam}` : '';
     }
 }
