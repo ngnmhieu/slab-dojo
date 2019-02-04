b24467697b5c59f18eac70ea4fc2d09914357a18
sberlin <sberlin@users.noreply.github.com>
2019-02-04 16:20:43 +0100

    [EDO-225] Fix tests of image conversion
    By using valid images as byte arrays

--- src/test/java/de/otto/teamdojo/web/rest/ImageResourceIntTest.java
+++ src/test/java/de/otto/teamdojo/web/rest/ImageResourceIntTest.java
@@ -8,14 +8,11 @@ import de.otto.teamdojo.service.ImageService;
 import de.otto.teamdojo.service.dto.ImageDTO;
 import de.otto.teamdojo.service.mapper.ImageMapper;
 import de.otto.teamdojo.web.rest.errors.ExceptionTranslator;
-import de.otto.teamdojo.service.dto.ImageCriteria;
 import de.otto.teamdojo.service.ImageQueryService;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,7 +27,6 @@ import org.springframework.util.Base64Utils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
-import java.util.ArrayList;
 
 import static de.otto.teamdojo.web.rest.TestUtil.createFormattingConversionService;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,23 +43,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = TeamdojoApp.class)
 public class ImageResourceIntTest {
 
+    private static final byte[] JPG_BLACK = new byte[] { -1, -40, -1, -32, 0, 16, 74, 70, 73, 70, 0, 1, 1, 1, 0, 72, 0, 72, 0, 0, -1, -37, 0, 67, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -37, 0, 67, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -55, 0, 17, 8, 0, 1, 0, 1, 3, 1, 17, 0, 2, 17, 1, 3, 17, 1, -1, -52, 0, 10, 0, 16, 16, 5, 1, 16, 17, 5, -1, -38, 0, 12, 3, 1, 0, 2, 17, 3, 17, 0, 63, 0, -52, 96, -1, -39 };
+    private static final byte[] JPG_WHITE = new byte[] { -1, -40, -1, -32, 0, 16, 74, 70, 73, 70, 0, 1, 1, 1, 0, 72, 0, 72, 0, 0, -1, -37, 0, 67, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -62, 0, 11, 8, 0, 1, 0, 1, 1, 1, 17, 0, -1, -60, 0, 20, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -38, 0, 8, 1, 1, 0, 1, 63, 16 };
+
     private static final String DEFAULT_NAME = "AAAAAAAAAA";
     private static final String UPDATED_NAME = "BBBBBBBBBB";
 
-    private static final byte[] DEFAULT_SMALL = TestUtil.createByteArray(1, "0");
-    private static final byte[] UPDATED_SMALL = TestUtil.createByteArray(2, "1");
+    private static final byte[] DEFAULT_SMALL = JPG_BLACK;
+    private static final byte[] UPDATED_SMALL = JPG_WHITE;
     private static final String DEFAULT_SMALL_CONTENT_TYPE = "image/jpg";
-    private static final String UPDATED_SMALL_CONTENT_TYPE = "image/png";
+    private static final String UPDATED_SMALL_CONTENT_TYPE = "image/jpg";
 
-    private static final byte[] DEFAULT_MEDIUM = TestUtil.createByteArray(1, "0");
-    private static final byte[] UPDATED_MEDIUM = TestUtil.createByteArray(2, "1");
+    private static final byte[] DEFAULT_MEDIUM = JPG_BLACK;
+    private static final byte[] UPDATED_MEDIUM = JPG_WHITE;
     private static final String DEFAULT_MEDIUM_CONTENT_TYPE = "image/jpg";
-    private static final String UPDATED_MEDIUM_CONTENT_TYPE = "image/png";
+    private static final String UPDATED_MEDIUM_CONTENT_TYPE = "image/jpg";
 
-    private static final byte[] DEFAULT_LARGE = TestUtil.createByteArray(1, "0");
-    private static final byte[] UPDATED_LARGE = TestUtil.createByteArray(2, "1");
+    private static final byte[] DEFAULT_LARGE = JPG_BLACK;
+    private static final byte[] UPDATED_LARGE = JPG_WHITE;
     private static final String DEFAULT_LARGE_CONTENT_TYPE = "image/jpg";
-    private static final String UPDATED_LARGE_CONTENT_TYPE = "image/png";
+    private static final String UPDATED_LARGE_CONTENT_TYPE = "image/jpg";
+
+    private static final byte[] EXPECTED_IMAGE_BLACK = new byte[] { -119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 2, 0, 0, 0, -112, 119, 83, -34, 0, 0, 0, 12, 73, 68, 65, 84, 120, -38, 99, -8, -1, -1, 63, 0, 5, -2, 2, -2, 51, 18, -107, 20, 0, 0, 0, 0, 73, 69, 78, 68, -82, 66, 96, -126 };
+    private static final byte[] EXPECTED_IMAGE_WHITE = new byte[] { -119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0, 58, 126, -101, 85, 0, 0, 0, 10, 73, 68, 65, 84, 120, -38, 99, 104, 0, 0, 0, -126, 0, -127, -38, 69, 8, 59, 0, 0, 0, 0, 73, 69, 78, 68, -82, 66, 96, -126 };
+    private static final String EXPECTED_CONTENT_TYPE = "image/png";
 
     @Autowired
     private ImageRepository imageRepository;
@@ -131,7 +134,6 @@ public class ImageResourceIntTest {
     }
 
     @Test
-    @Ignore
     @Transactional
     public void createImage() throws Exception {
         int databaseSizeBeforeCreate = imageRepository.findAll().size();
@@ -148,12 +150,12 @@ public class ImageResourceIntTest {
         assertThat(imageList).hasSize(databaseSizeBeforeCreate + 1);
         Image testImage = imageList.get(imageList.size() - 1);
         assertThat(testImage.getName()).isEqualTo(DEFAULT_NAME);
-        assertThat(testImage.getSmall()).isEqualTo(DEFAULT_SMALL);
-        assertThat(testImage.getSmallContentType()).isEqualTo(DEFAULT_SMALL_CONTENT_TYPE);
-        assertThat(testImage.getMedium()).isEqualTo(DEFAULT_MEDIUM);
-        assertThat(testImage.getMediumContentType()).isEqualTo(DEFAULT_MEDIUM_CONTENT_TYPE);
-        assertThat(testImage.getLarge()).isEqualTo(DEFAULT_LARGE);
-        assertThat(testImage.getLargeContentType()).isEqualTo(DEFAULT_LARGE_CONTENT_TYPE);
+        assertThat(testImage.getSmall()).containsExactly(EXPECTED_IMAGE_BLACK);
+        assertThat(testImage.getSmallContentType()).isEqualTo(EXPECTED_CONTENT_TYPE);
+        assertThat(testImage.getMedium()).containsExactly(EXPECTED_IMAGE_BLACK);
+        assertThat(testImage.getMediumContentType()).isEqualTo(EXPECTED_CONTENT_TYPE);
+        assertThat(testImage.getLarge()).containsExactly(EXPECTED_IMAGE_BLACK);
+        assertThat(testImage.getLargeContentType()).isEqualTo(EXPECTED_CONTENT_TYPE);
     }
 
     @Test
@@ -177,7 +179,6 @@ public class ImageResourceIntTest {
     }
 
     @Test
-    @Ignore
     @Transactional
     public void checkNameIsRequired() throws Exception {
         int databaseSizeBeforeTest = imageRepository.findAll().size();
@@ -313,7 +314,6 @@ public class ImageResourceIntTest {
     }
 
     @Test
-    @Ignore
     @Transactional
     public void updateImage() throws Exception {
         // Initialize the database
@@ -345,16 +345,15 @@ public class ImageResourceIntTest {
         assertThat(imageList).hasSize(databaseSizeBeforeUpdate);
         Image testImage = imageList.get(imageList.size() - 1);
         assertThat(testImage.getName()).isEqualTo(UPDATED_NAME);
-        assertThat(testImage.getSmall()).isEqualTo(UPDATED_SMALL);
-        assertThat(testImage.getSmallContentType()).isEqualTo(UPDATED_SMALL_CONTENT_TYPE);
-        assertThat(testImage.getMedium()).isEqualTo(UPDATED_MEDIUM);
-        assertThat(testImage.getMediumContentType()).isEqualTo(UPDATED_MEDIUM_CONTENT_TYPE);
-        assertThat(testImage.getLarge()).isEqualTo(UPDATED_LARGE);
-        assertThat(testImage.getLargeContentType()).isEqualTo(UPDATED_LARGE_CONTENT_TYPE);
+        assertThat(testImage.getSmall()).isEqualTo(EXPECTED_IMAGE_WHITE);
+        assertThat(testImage.getSmallContentType()).isEqualTo(EXPECTED_CONTENT_TYPE);
+        assertThat(testImage.getMedium()).containsExactly(EXPECTED_IMAGE_WHITE);
+        assertThat(testImage.getMediumContentType()).isEqualTo(EXPECTED_CONTENT_TYPE);
+        assertThat(testImage.getLarge()).isEqualTo(EXPECTED_IMAGE_WHITE);
+        assertThat(testImage.getLargeContentType()).isEqualTo(EXPECTED_CONTENT_TYPE);
     }
 
     @Test
-    @Ignore
     @Transactional
     public void updateNonExistingImage() throws Exception {
         int databaseSizeBeforeUpdate = imageRepository.findAll().size();
