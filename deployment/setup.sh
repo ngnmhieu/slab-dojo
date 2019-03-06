3449ffc40be8dae2702589bba38a8db5340bce38
sberlin <sberlin@users.noreply.github.com>
2019-03-06 17:54:40 +0100

    EDO-239 Handle dynamic subdomain creation

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -11,6 +11,7 @@ read_var() {
 read_var OC_NAMESPACE "OpenShift namespace: "
 read_var OC_PROJECT_NAME "OpenShift project name: "
 read_var OC_PROJECT_DESC "OpenShift project description: "
+read_var OC_SUBDOMAIN "OpenShift application subdomain: "
 read_var DB_USER "Database user: "
 read_var DB_PASSWORD "Database password: " silent
 read_var DOCKER_CFG_REGISTRY "Docker registry name: "
@@ -23,24 +24,24 @@ oc new-project "${OC_NAMESPACE}" --display-name="${OC_PROJECT_NAME}" --descripti
 oc project "${OC_NAMESPACE}"
 
 create() {
-    oc create -f "${SCRIPT_DIR}/${1}"
+    oc create -f - <&0
 }
 
 create_instance() {
     local instancesuffix="${1}"
     echo "Creating instance with suffix '${instancesuffix}'"
-    create "persistentVolumeClaims/pg-data${instancesuffix}.yml"
-    create "deployments/postgres${instancesuffix}.yml"
-    create "deployments/teamdojo${instancesuffix}.yml"
-    create "services/postgres${instancesuffix}.yml"
-    create "services/teamdojo${instancesuffix}.yml"
-    create "routes/teamdojo${instancesuffix}.yml"
+    create < "${SCRIPT_DIR}/persistentVolumeClaims/pg-data${instancesuffix}.yml"
+    create < "${SCRIPT_DIR}/deployments/postgres${instancesuffix}.yml"
+    create < "${SCRIPT_DIR}/deployments/teamdojo${instancesuffix}.yml"
+    create < "${SCRIPT_DIR}/services/postgres${instancesuffix}.yml"
+    create < "${SCRIPT_DIR}/services/teamdojo${instancesuffix}.yml"
+    create < <(sed 's/TEAMDOJO_SUBDOMAIN/'"${OC_SUBDOMAIN}${instancesuffix}"'/g' "${SCRIPT_DIR}/routes/teamdojo${instancesuffix}.yml")
 }
 
 oc secrets new-dockercfg "registry-dockercfg" --docker-server="${DOCKER_CFG_REGISTRY}" --docker-username="${DOCKER_CFG_USERNAME}" --docker-email="${DOCKER_CFG_EMAIL}" --docker-password="${DOCKER_CFG_TOKEN}"
 oc secrets new-basicauth "database-creds" --username="${DB_USER}" --password="${DB_PASSWORD}"
-create "serviceaccounts/bamboodeployer.yml"
-create "rolebindings/edit.yml"
+create < "${SCRIPT_DIR}/serviceaccounts/bamboodeployer.yml"
+create < "${SCRIPT_DIR}/rolebindings/edit.yml"
 
 create_instance ""
 create_instance "-int"
